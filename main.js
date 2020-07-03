96027ecf605caf8640493f1848da152cda3d7a73
trywin0 <67023288+trywin0@users.noreply.github.com>
2020-07-04 00:51:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+/*TODO____________
+clean up the auto unmute : 16-30
+clean up reminders : 32-47
+finish learning mongodb and replace quick.db with it*/
+
 const Discord = require("discord.js"),
     client = new Discord.Client({
         disableMentions: "everyone"
@@ -7,29 +12,6 @@ const Discord = require("discord.js"),
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.events = new Discord.Collection();
-const Reset = "\x1b[0m",
-    Bright = "\x1b[1m",
-    Dim = "\x1b[2m",
-    Underscore = "\x1b[4m",
-    Blink = "\x1b[5m",
-    Reverse = "\x1b[7m",
-    Hidden = "\x1b[8m",
-    FgBlack = "\x1b[30m",
-    FgRed = "\x1b[31m",
-    FgGreen = "\x1b[32m",
-    FgYellow = "\x1b[33m",
-    FgBlue = "\x1b[34m",
-    FgMagenta = "\x1b[35m",
-    FgCyan = "\x1b[36m",
-    FgWhite = "\x1b[37m",
-    BgBlack = "\x1b[40m",
-    BgRed = "\x1b[41m",
-    BgGreen = "\x1b[42m",
-    BgYellow = "\x1b[43m",
-    BgBlue = "\x1b[44m",
-    BgMagenta = "\x1b[45m",
-    BgCyan = "\x1b[46m",
-    BgWhite = "\x1b[47m";
 const db = require("quick.db");
 setInterval(() => {
             client.guilds.cache.forEach(g => {
@@ -72,7 +54,7 @@ fs.readdir("./commands/", (err, files) => {
     files.forEach(file => {
         if (!file.endsWith(".js")) return;
         let props = require(`./commands/${file}`);
-        console.log(FgGreen, "Successfully loaded " + file)
+        console.log("Successfully loaded " + file)
         let commandName = file.split(".")[0];
         client.commands.set(commandName, props);
     });
@@ -82,14 +64,13 @@ fs.readdir('./events/', (err, files) => {
     files.forEach(file => {
         let eventFunc = require(`./events/${file}`);
 
-        console.log(FgGreen, "Successfully loaded " + file)
+        console.log("Successfully loaded " + file)
         let eventName = file.split(".")[0];
         client.on(eventName, (...args) => eventFunc.run(client, ...args));
     });
 });
 
 client.on("ready", () => {
-    console.log(FgBlue, "\n\n\n" + client.user.username + " is online.")
-    process.stdout.write("\033]0;" + client.user.username + " \007");
+    console.log(client.user.username + " is online.")
 });
 client.login(config.token)
